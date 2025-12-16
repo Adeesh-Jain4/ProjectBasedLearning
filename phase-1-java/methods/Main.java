591bcca837c349e5f6ca27df6441353280697c4a
Adeesh <adeeshjain12345@gmail.com>
2025-12-17 01:34:36 +0530

    First Java methods practice

---
+++ phase-1-java/methods/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        printWelcome();
+    }
+
+    public static void printWelcome() {
+        System.out.println("Learning Java + Git + APIs");
+    }
+}
